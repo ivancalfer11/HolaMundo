9f5ff3accbb6e4043c6c831cf579dd42ab25f2a2
ivanozil11 <ivancalfer1@gmail.com>
2016-10-03 13:02:30 +0200

    he añadido mi edad

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,6 @@
 public class HolaMundo{
      public static void main (String args[]){
          System.out.println("Ivan Calvo Fernandez");
-		 int num;
-		 num=100;
-		 System.out.println(num);
-		 System.out.println("hala Madrid!!!!!!");
+		 System.out.println("Tengo 20 anos");
      }
 }
