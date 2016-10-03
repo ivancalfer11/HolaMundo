81fa6ce0d45b69f066d93dd32182df9ad681d6ee
ivanozil11 <ivancalfer1@gmail.com>
2016-10-03 12:28:35 +0200

    Creando repositorio adecuado para programa

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo{
+     public static void main (String args[]){
+         System.out.println("Ivan Calvo Fernandez");
+		 int num;
+		 num=100;
+		 System.out.println(num);
+		 System.out.println("hala Madrid!!!!!!");
+     }
+}
